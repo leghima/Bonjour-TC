2e30154ceb909ddd5204b881cc35ef576b9876a3
leghima <arezkileghima0@gmail.com>
2026-05-05 22:21:37 +0200

    fix cors complet3

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API = "https://bonjour-tc-backend.onrender.com/";
+const API = "https://bonjour-tc-backend.onrender.com";
 
 
 // ── Calcul automatique des prochains passages ────────────────────────────────
